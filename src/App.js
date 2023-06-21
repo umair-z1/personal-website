3cbbdbb6066aa87dff16f7fcabf43e433e7a961e
Umair <uamin6@gatech.edu>
2023-06-21 18:52:30 -0400

    changed about

--- src/App.js
+++ src/App.js
@@ -2,16 +2,27 @@ import React from 'react';
 import './App.css';
 import linkedin from './resources/linkedin.png'
 import github from './resources/github.png'
-import email from './resources/email.png'
+import about from './resources/about.png'
 import { motion } from "framer-motion";
 
 function App() {
+
+    const [showAbout, setShowAbout] = React.useState(false);
+    const [initial, setInitial] = React.useState(true);
+    const toggleAbout = () => {setInitial(false); setShowAbout(!showAbout);}
+
     return (
         <div className="App-Container">
+            <div className={("About " + (initial ? "" : (showAbout ? "FadeIn" : "FadeOut")))} onClick={toggleAbout}>
+                <div className="about-container">
+                    <div className='about-text'>Hello! My name is Umair and I am a junior studying computer science at Georgia Tech.</div>
+                    <motion.button className='about-backArrow'
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.9 }}>← </motion.button>
+                </div>
+            </div>
             <div className="App">
-                <FadeIn>
-                    <h1 className="name-text">umair amin</h1>
-                </FadeIn>
+                <h1 className="name-text">umair amin</h1>
                 <div className="icons">
                     <Icon
                         link="https://github.com/umair-z1"
@@ -21,16 +32,16 @@ function App() {
                         link="http://www.linkedin.com/in/umair-amin-t"
                         image={linkedin}
                         name="linkedin" />
-                    <div className="icon">
+                    <div className="icon" id="about">
                         <motion.button
                             className="motion-button"
                             whileHover={{ scale: 1.1 }}
                             whileTap={{ scale: 0.9 }}
-                            onClick={() => alert("Coming Soon!")}
+                            onClick={toggleAbout}
                         >
-                            <img className="icon-image" src={email} alt="email" />
+                            <img className="icon-image" src={about} alt="email" />
                         </motion.button>
-                        <p className="icon-text">contact</p>
+                        <p className="icon-text">about</p>
                     </div>
                 </div>
             </div>
@@ -38,25 +49,9 @@ function App() {
     );
 }
 
-function FadeIn({ children }) {
-    return (
-        <motion.div
-            intial="hidden"
-            // whileInView="visible"
-            // viewport={{ once: true }}
-            // transition={{
-            //     visible: {opacity: 1, scale: 1},
-            //     hidden: {opacity: 0, scale: 0}
-            // }}
-            >
-            { children }
-        </motion.div>
-    )
-}
-
 function Icon(props) {
     return (
-        <a className="icon" href={props.link}>
+        <a className="icon" id={props.name} href={props.link}>
             <motion.button
                 className="motion-button"
                 whileHover={{ scale: 1.1 }}

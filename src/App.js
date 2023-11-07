763840ce6f2843cd7b3b67f0954724fd71535fbb
Umair <uamin6@gatech.edu>
2023-11-06 20:58:04 -0500

    edited links, added pages

--- src/App.js
+++ src/App.js
@@ -1,57 +1,101 @@
-import React from 'react';
+import { React, useRef } from 'react';
 import './App.css';
 import linkedin from './resources/linkedin.png'
 import github from './resources/github.png'
 import about from './resources/about.png'
+import shapes from './resources/shapes.png'
 import { motion } from "framer-motion";
 
 function App() {
 
-    const [showAbout, setShowAbout] = React.useState(false);
-    const [initial, setInitial] = React.useState(true);
-    const toggleAbout = () => {setInitial(false); setShowAbout(!showAbout);}
+    const aboutref = useRef()
+    const deindicaref = useRef()
 
     return (
+        <>
         <div className="App-Container">
-            <div className={("About " + (initial ? "" : (showAbout ? "FadeIn" : "FadeOut")))} onClick={toggleAbout}>
-                <div className="about-container">
-                    <div className='about-text'>Hello! My name is Umair and I am a junior studying computer science at Georgia Tech.</div>
-                    <motion.button className='about-backArrow'
-                    whileHover={{ scale: 1.1 }}
-                    whileTap={{ scale: 0.9 }}>← </motion.button>
-                </div>
-            </div>
             <div className="App">
                 <h1 className="name-text">umair amin</h1>
                 <div className="icons">
+
+
                     <Icon
                         link="https://github.com/umair-z1"
                         image={github}
                         name="github" />
+
+
                     <Icon
                         link="http://www.linkedin.com/in/umair-amin-t"
                         image={linkedin}
                         name="linkedin" />
+
+
+                    <div className="icon" id="project">
+                        <motion.button
+                            className="motion-button"
+                            whileHover={{ scale: 1.1 }}
+                            whileTap={{ scale: 0.9 }}
+                            onClick={() => deindicaref.current.scrollIntoView({behavior: "smooth"})}
+                        >
+                            <img className="icon-image" src={shapes} alt="projects" />
+                        </motion.button>
+                        <p className="icon-text">projects</p>
+                    </div>
+
+
                     <div className="icon" id="about">
                         <motion.button
                             className="motion-button"
                             whileHover={{ scale: 1.1 }}
                             whileTap={{ scale: 0.9 }}
-                            onClick={toggleAbout}
+                            onClick={() => aboutref.current.scrollIntoView({behavior: "smooth"})}
                         >
-                            <img className="icon-image" src={about} alt="email" />
+                            <img className="icon-image" src={about} alt="about" />
                         </motion.button>
                         <p className="icon-text">about</p>
                     </div>
+
+
                 </div>
             </div>
         </div>
+        <div className="purple-page" ref={aboutref}>
+            <div className="about-title">
+                About Me
+            </div>
+            <div className="about-body">
+                Hello, my name is Umair. I am a third year Computer Science student at Georgia Institute of Technology with 
+                threads/concentrations in information internetworks and intelligence. I am currently pursuing a career in software
+                engineering because I enjoy the creative challenge of transforming ideas into functional applications that can
+                postively impact someone. I am particularly interested in full-stack development - from designing user interfaces
+                and experiences to building robust back-end systems and databases. What drives me forward is being able to see how
+                each component of a program works and fits into a cohesive whole.
+            </div>
+        </div>
+        <div className="gray-page" ref={deindicaref}>
+            <div className="about-title">
+                Deindica
+            </div>
+            <div className="about-body">
+                Deindica is a full-stack application in which users can boost their math and problem-solving skills. Users can select
+                a subject that they want to learn, and they will be given problems in accordance with their skill levels. The skill
+                level is determined by a dynamic rating system, in which a user is put against a problem, and both the problem and user
+                ratings are adjusted based on the answer that the user gave. I have partnered up with the University of Texas at Dallas
+                math club's former president to create several problems. We also gave access to some of the math club students so that
+                they could test Deindica and give feedback.
+            </div>
+            <div className="about-body">
+                Status: Development complete, not yet published
+            </div>
+        </div>
+        </>
     );
 }
 
 function Icon(props) {
     return (
-        <a className="icon" id={props.name} href={props.link}>
+        <a className="icon" id={props.name} href={props.link} target="_blank" rel="noreferrer">
             <motion.button
                 className="motion-button"
                 whileHover={{ scale: 1.1 }}
@@ -63,18 +107,5 @@ function Icon(props) {
         </a>
     )
 }
-
-// function Backdrop({children, onClick}) {
-//     return (
-//         <motion.div
-//             className="backdrop"
-//             onclick={onClick}
-//             initial={{ opacity: 0 }}
-//             animate={{ opacity: 1 }}
-//             exit= {{ opacity: 0 }} >
-//             {children}
-//         </motion.div>
-//     );
-// }
 
 export default App;
